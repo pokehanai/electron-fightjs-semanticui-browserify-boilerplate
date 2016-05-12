5c4d810fb136715b1ea77d0150a30de4feb912c2
HANAI Tohru <hanai@pokelabo.co.jp>
2016-05-12 14:15:11 +0900

    Update flightjs debug mode handling

--- src/renderer/scripts/app.js
+++ src/renderer/scripts/app.js
@@ -3,10 +3,14 @@
 import "babel-polyfill";
 import $ from "jquery";
 import { advice, component, compose, debug, logger, registry, utils } from 'flightjs'
+import { remote } from 'electron'
 
 window.$ = $;
 
-debug.enable(true);
+if (remote.process.env.NODE_ENV == 'develop') {
+  debug.enable(true);
+  debug.events.logAll();
+}
 compose.mixin(registry, [advice.withAdvice]);
 
 require('./data/data').attachTo(document);
